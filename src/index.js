5e9b5a24332582cab1814e487f1307e8a2bbfdc7
Chiran Gurung <grgchiran08@yahoo.com>
2023-08-07 20:56:35 -0400

    practice star rating

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StarRating from "./StarRating";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <>
+      <StarRating
+        color="blue"
+        maxRating={10}
+        onSetMovieRating={setMovieRating}
+      />
+      <p>This movie was rated {movieRating} stars.</p>
+    </>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={1} />
+    <Test />
   </React.StrictMode>
 );
